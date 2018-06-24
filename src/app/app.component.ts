12318ecccd30f40fd8a78ed251a7cb8d11863d18
Saurabh Pawar <saurabhpawar82220@gmail.com>
2018-06-24 00:54:22 -0800

    homework3
    1.
    5.

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ 
+  name1="Assignment 2";
+  name2="Marvellous";
+}
